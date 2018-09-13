459a3581bd6d0265c719a05f94081233cf26d981
adeel qureshi <qureshia@ufl.edu>
2018-09-13 13:08:42 -0400

    lab1 initial commit

---
+++ lab1/Debug/main.d
@@ -0,0 +1,97 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/msp.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/msp432p401r.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+main.obj: C:/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/driverlib.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/adc14.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdbool.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/aes256.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/comp_e.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/cpu.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/crc32.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/cs.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/dma.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/eusci.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/flash.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/fpu.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/gpio.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/i2c.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/interrupt.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/mpu.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/pcm.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/pmap.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/pss.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/ref_a.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/reset.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/rom.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/rom_map.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/rtc_c.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/spi.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/sysctl.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/systick.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/timer32.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/timer_a.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/uart.h
+main.obj: D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/wdt_a.h
+
+../main.c: 
+C:/ti/ccsv8/ccs_base/arm/include/msp.h: 
+C:/ti/ccsv8/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/driverlib.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/adc14.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdbool.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/aes256.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/comp_e.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/cpu.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/crc32.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/cs.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/dma.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/eusci.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/flash.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/fpu.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/gpio.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/i2c.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/interrupt.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/mpu.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/pcm.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/pmap.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/pss.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/ref_a.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/reset.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/rom.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/rom_map.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/rtc_c.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/spi.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/sysctl.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/systick.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/timer32.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/timer_a.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/uart.h: 
+D:/uf2018/eel4930-microp2/labs/lab1/lab1/lab1/BoardSupportPackage/DriverLib/wdt_a.h: 
